2701bb7178c29159e8c0ab88a767e7e4fb9e7bd7
Q.Toan <toanquoc2003@gmail.com>
2025-05-30 16:04:19 +0700

    add base container

---
+++ Assets/MainGame/Scripts/Player/Player.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public LayerMask layerContainer;
+    private Container container;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray ray;
+            if (Input.touchCount > 0)
+            {
+                ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            }
+            else
+            {
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            }
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerContainer))
+            {
+                Debug.Log(hit.collider.name);
+                container = hit.collider.gameObject.GetComponent<Container>();
+                container.MoveContainNer();
+            }
+            else{
+                Debug.Log("0");
+            }
+
+        }
+    }
+
+}
